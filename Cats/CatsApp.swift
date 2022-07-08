feb856a70cb662db57bacf58c333ca0d886ba714
Taeho-Kim <taehokim@hicare.net>
2022-07-08 21:09:50 +0900

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by 김태호 on 2022/07/08.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
