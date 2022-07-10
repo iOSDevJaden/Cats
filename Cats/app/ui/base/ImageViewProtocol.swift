3deb294ab815c886fefe8f674897de414455fdab
Taeho-Kim <taehokim@hicare.net>
2022-07-10 20:19:45 +0900

    [Feat] Created ImageViewProtocol
    - To prevent implementing same functions
    - Contains a function that create an Image instance of SwiftUI from image url

---
+++ Cats/app/ui/base/ImageViewProtocol.swift
@@ -0,0 +1,25 @@
+//
+//  ImageViewProtocol.swift
+//  Cats
+//
+//  Created by 김태호 on 2022/07/10.
+//
+
+import Foundation
+import SwiftUI
+
+protocol ImageViewProtocol {
+    func getImage(imageUrl: String?) -> Image?
+}
+
+extension ImageViewProtocol {
+    func getImage(imageUrl: String?) -> Image? {
+        guard let imageUrl = imageUrl,
+              let imageUrl = URL(string: imageUrl),
+              let imageData = try? Data(contentsOf: imageUrl),
+              let uiImage = UIImage(data: imageData) else {
+            return nil
+        }
+        return Image(uiImage: uiImage)
+    }
+}
