3e33919b154ed333ac351d042342ce344da7fa66
Taeho-Kim <taehokim@hicare.net>
2022-07-10 22:34:27 +0900

    [Refactor] Vote ViewModel first draft
    - minimun functionalities.
    1. Get image(s) from network api using Combine framework

--- Cats/app/ui/VoteView/VoteViewModel.swift
+++ Cats/app/ui/VoteView/VoteViewModel.swift
@@ -5,7 +5,52 @@
 //  Created by 김태호 on 2022/07/08.
 //
 
+import Combine
 import Foundation
 
 class VoteViewModel: ObservableObject {
+    private lazy var imagesService = ImagesService()
+    private lazy var voteService = VoteService()
+    
+    private var cancellable = Set<AnyCancellable>()
+    
+    @Published var images: [ImageFull] = []
+    
+    func getSingleImage() {
+        imagesService.getSingleImage()
+            .receive(on: DispatchQueue.main)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .tryMap { $0.data }
+            .decode(type: [ImageFull].self, decoder: JSONDecoder())
+            .sink(
+                receiveCompletion: {
+                    print("Receive Completion \($0)")
+                },
+                receiveValue: { [weak self] images in
+                    self?.images = images
+                    self?.images.forEach {
+                        print("Image Url \($0.url ?? "Nothing")")
+                    }
+                })
+            .store(in: &cancellable)
+    }
+    
+    func getAllImages() {
+        imagesService.getAllImages(limit: 10)
+            .receive(on: DispatchQueue.main)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .tryMap { $0.data }
+            .decode(type: [ImageFull].self, decoder: JSONDecoder())
+            .sink(
+                receiveCompletion: {
+                    print("Receive Completion \($0)")
+                },
+                receiveValue: { [weak self] images in
+                    self?.images = images
+                    self?.images.forEach {
+                        print("Image Url \($0.url ?? "Nothing")")
+                    }
+                })
+            .store(in: &cancellable)
+    }
 }
