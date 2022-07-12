4487b755ecaf3cc1f6fbd1c325e6056e7ed1089e
Taeho-Kim <taehokim@hicare.net>
2022-07-12 22:21:21 +0900

    [Refactor] Network Response and Request models seperated.

---
+++ Cats/app/models/response/MessageResponse.swift
@@ -0,0 +1,13 @@
+//
+//  MessageResponse.swift
+//  Cats
+//
+//  Created by 김태호 on 2022/07/12.
+//
+
+import Foundation
+
+struct MessageResponse: Codable {
+    let message: String
+    let id: Int
+}
