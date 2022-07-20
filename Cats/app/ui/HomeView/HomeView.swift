85cf0728e0f8fe93faecfbf70ebb4dd28f2c40c0
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-07-20 23:53:59 +0900

    [Refactor] Home View applied to downloading favourite and vote-up image urls.

--- Cats/app/ui/HomeView/HomeView.swift
+++ Cats/app/ui/HomeView/HomeView.swift
@@ -7,9 +7,52 @@
 
 import SwiftUI
 
+/**
+ * MARK: - Home View
+ *  Home View shows User's Favourite cat pictures in large scale on the section first.
+ *  And then shows Vote-up cat pictures in large scale.
+ *  No order pictures considered.
+ */
+
 struct HomeView: View {
+    @ObservedObject private var vm = HomeViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            if vm.favourites.isEmpty {
+                Text("No favourite cat pictures")
+            } else {
+                Section(
+                    content: {
+                        ForEach(vm.favourites) { image in
+                            if let url = image.image?.url {
+                                AsyncImgView(url)
+                            }
+                        }
+                    },
+                    header: {
+                        Text("My Favourite Views")
+                    })
+            }
+            
+            if vm.voteUps.isEmpty {
+                Text("No Vote-up cat pictures")
+            } else {
+                Section(
+                    content: {
+                        ForEach(vm.voteUps) { image in
+                            if let url = image.url {
+                                AsyncImgView(url)
+                            }
+                        }
+                    },
+                    header: {
+                        Text("My Vote-up Views")
+                    })
+            }
+        }
+        .onAppear(perform: vm.getFavouriteImages)
+        .onAppear(perform: vm.getVoteUpImages)
     }
 }
 
