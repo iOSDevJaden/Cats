ea444aecc602c36a7d765153240e1b1ae1ff5d9a
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-07-20 23:12:55 +0900

    [Feat] Profile View, Home View created
    - Home View shows user's favourite cats
    - Profile View shows user's private settings

---
+++ Cats/app/ui/HomeView/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Cats
+//
+//  Created by 김태호 on 2022/07/20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
