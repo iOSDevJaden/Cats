17d5a41aeea3c17c12a98db3643183f429d6d2d7
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-07-23 13:14:41 +0900

    [Feat] ImageService Created

--- Cats/app/api+services/services/ImagesService.swift
+++ Cats/app/api+services/services/ImagesService.swift
@@ -5,20 +5,62 @@
 //  Created by 김태호 on 2022/07/10.
 //
 
+import Combine
 import Foundation
  
-struct ImagesService {
+class ImagesService {
+    private var cancellable = Set<AnyCancellable>()
     private let imagesApi = ImagesApi()
     
-    func getSingleImage() -> URLSession.DataTaskPublisher {
-        return URLSession.shared.dataTaskPublisher(for: imagesApi.getSingleImage())
+    func getImages() -> AnyPublisher<[ImageRes], Error> {
+        return Deferred {
+            Future { promise in
+                URLSession.shared.dataTaskPublisher(for: self.imagesApi.getSingleImage())
+                    .receive(on: DispatchQueue.global(qos: .background))
+                    .subscribe(on: DispatchQueue.main)
+                    .map(\.data)
+                    .decode(type: [ImageRes].self, decoder: JSONDecoder())
+                    .sink(
+                        receiveCompletion: {
+                            switch($0) {
+                            case .failure(let error):
+                                promise(.failure(error))
+                            case .finished:
+                                return
+                            }
+                        },
+                        receiveValue: {
+                            promise(.success($0))
+                        })
+                    .store(in: &self.cancellable)
+            }
+        }
+        .eraseToAnyPublisher()
     }
     
-    func getAllImages(limit: Int) -> URLSession.DataTaskPublisher {
-        return URLSession.shared.dataTaskPublisher(for: imagesApi.getAllPublicImages(limit: limit))
-    }
-    
-    func getImage(by id: String) -> URLSession.DataTaskPublisher {
-        return URLSession.shared.dataTaskPublisher(for: imagesApi.getImage(by: id))
+    func getImages(id imageId: String) -> AnyPublisher<ImageRes, Error> {
+        return Deferred {
+            Future { promise in
+                URLSession.shared.dataTaskPublisher(for: self.imagesApi.getSingleImage())
+                    .receive(on: DispatchQueue.global(qos: .background))
+                    .subscribe(on: DispatchQueue.main)
+                    .map(\.data)
+                    .decode(type: ImageRes.self, decoder: JSONDecoder())
+                    .sink(
+                        receiveCompletion: {
+                            switch($0) {
+                            case .failure(let error):
+                                promise(.failure(error))
+                            case .finished:
+                                return
+                            }
+                        },
+                        receiveValue: {
+                            promise(.success($0))
+                        })
+                    .store(in: &self.cancellable)
+            }
+        }
+        .eraseToAnyPublisher()
     }
 }
