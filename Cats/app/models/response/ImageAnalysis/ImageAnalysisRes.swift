0b5826b54ab554234acf6c361fd24148d54f022f
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-07-24 23:12:15 +0900

    [Doc] Documentation of Network Response through Uploading Image has been changed.
    - The Cat API documentation has not been updated.

--- Cats/app/models/response/ImageAnalysis/ImageAnalysisRes.swift
+++ Cats/app/models/response/ImageAnalysis/ImageAnalysisRes.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 
-struct ImageAnalysisRes: Codable {
+/**
+ * MARK: - Wrong Information
+ * The following structure is not correct information
+ * https://docs.thecatapi.com/api-reference/models/imageanalysis
+ * struct ImageAnalysisRes: Codable {
     let imageId: String
     let labels: [String]
     let moderationLabels: [String]
@@ -22,5 +26,27 @@ struct ImageAnalysisRes: Codable {
         case vendor = "vendor"
         case approved = "approved"
         case rejected = "rejected"
+    }
+} */
+
+struct ImageAnalysisRes: Codable {
+    let id: String
+    let url: String
+    let subId: String?
+    let width: Int
+    let height: Int
+    let originalFilename: String
+    let pending: Int
+    let apporoved: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id = " id"
+        case url = " url"
+        case subId = " sub_id"
+        case width = " width"
+        case height = " height"
+        case originalFilename = " original_filename"
+        case pending = " pending"
+        case apporoved = " apporoved"
     }
 }
