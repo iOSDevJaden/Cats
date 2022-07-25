b4fd45c641e49a2384207fe92b418e2f82c7f12c
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-07-26 05:39:42 +0900

    [Feat] User Settings, Preference Properties added.

--- Cats/app/cache/UserInfoCache.swift
+++ Cats/app/cache/UserInfoCache.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class UserInfoCache: Identifiable {
     static var shared = UserInfoCache()
+    lazy var userDefault = UserDefaults.standard
     
     private init() { }
     
@@ -20,5 +21,49 @@ class UserInfoCache: Identifiable {
         return deviceId
     }()
     
-    // Settings for page
+    // Current Page
+    func getCurrentPage() -> Int {
+        if let currentImagePage = userDefault.value(forKey: Const.currentImagePageKey) as? Int {
+            return currentImagePage
+        }
+        return Const.defualtNumberOfImage
+    }
+    
+    func setCurrentPage(page: Int) {
+        userDefault.set(page, forKey: Const.currentImagePageKey)
+    }
+    
+    // Furthest Page
+    func getMaximumPage() -> Int {
+        if let lastImagePage = userDefault.value(forKey: Const.lastImagePageKey) as? Int {
+            return lastImagePage
+        }
+        return Const.defualtMaximumPage
+    }
+    
+    func updateMaximumPage(page: Int) {
+        userDefault.set(page, forKey: Const.lastImagePageKey)
+    }
+    
+    // Number of cat pictures in the page
+    // Home and Search View
+    func getNumberOfImage() -> Int {
+        if let numberOfImage = userDefault.value(forKey: Const.numberOfImage) as? Int {
+            return numberOfImage
+        }
+        return Const.defualtNumberOfImage
+    }
+    
+    func setNumberOfImage(_ number: Int) {
+        userDefault.set(number, forKey: Const.numberOfImage)
+    }
+    
+    enum Const {
+        static let defualtNumberOfImage = 15
+        static let defualtMaximumPage = 0
+        static let defualtCurrentImagePage = 0
+        static let lastImagePageKey = "LastImagePageKey"
+        static let currentImagePageKey = "CurrentImagePageKey"
+        static let numberOfImage = "NumberOfImage"
+    }
 }
