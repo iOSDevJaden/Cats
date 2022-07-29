f0ae126404ab892b6f836b3dcf8f835d572c13e9
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-07-30 06:09:57 +0900

    [Refactor] ProfileView shares the same view model with settings view
    - SettingsViewModel shared.

--- Cats/app/ui/SubViews/ProfileView/ProfileView.swift
+++ Cats/app/ui/SubViews/ProfileView/ProfileView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var vm: SettingsViewModel
+    
     var body: some View {
         HStack(spacing: 0) {
             Image(systemName: "person")
@@ -17,13 +19,15 @@ struct ProfileView: View {
                 .clipShape(Circle())
                 .shadow(radius: 10)
                 .overlay(Circle().stroke(Color.red, lineWidth: 5))
+                .padding(.vertical, 10)
 
             VStack(alignment: .leading, spacing: 10) {
                 HStack {
                     Text("Name")
                 }
                 HStack {
-                    Text("Sub ID")
+                    Text(vm.getUserId())
+                        .font(.caption)
                 }
             }
             .font(.title2)
@@ -37,5 +41,6 @@ struct ProfileView: View {
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+            .environmentObject(SettingsViewModel())
     }
 }
