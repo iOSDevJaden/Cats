bf1da9e381f36fc67fb479f7ac11ef2773fab6c4
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-08-11 20:09:59 +0900

    [Test] Making UrlRequestBuilder testing url, url path

--- CatsTests/Util/TestingRequestBuilderTest.swift
+++ CatsTests/Util/TestingRequestBuilderTest.swift
@@ -6,23 +6,79 @@
 //
 
 import XCTest
-@testable import Cats
+
+struct UrlRequestBuilder {
+    var url: URL?
+    var path: String?
+    
+    init(_ url: URL? = nil,
+         _ path: String? = nil) {
+        self.url = url
+        self.path = path
+    }
+    
+    func setUrl(url: URL?) -> Self {
+        UrlRequestBuilder(url, self.path)
+    }
+    
+    func setPath(path: String?) -> Self {
+        UrlRequestBuilder(self.url, path)
+    }
+}
 
 class TestingRequestBuilderTest: XCTestCase {
-
-    private let url = URL.baseUrl.appendingPathComponent(String.apiVersion)
+    private let testOnlyUrl = URL(string: "https://example.com")
+    private let testPath = "/testPath"
     
-    func testBasicRequestBuilderTest() {
-        let requestBuilder = RequestBuilder()
-            .setPath(path: "")
-            .setMethod(method: .get)
-            .build()
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.setValue(String.apiKey, forHTTPHeaderField: "x-api-key")
-        
-        XCTAssertEqual(requestBuilder, request)
+    func test_urlRequestBuilderMemberUrlIsNil() {
+        let requestBuilder = UrlRequestBuilder()
+        XCTAssertNil(requestBuilder.url)
     }
-
+    
+    func test_urlRequestBuilderMemberUrlIsNotNil() {
+        let requestBuilder = UrlRequestBuilder(testOnlyUrl)
+        XCTAssertNotNil(requestBuilder.url)
+    }
+    
+    func test_urlRequestBuilderSetUrlIsNil() {
+        let requestBuilder = UrlRequestBuilder()
+            .setUrl(url: nil)
+        
+        XCTAssertNil(requestBuilder.url)
+    }
+    
+    func test_urlRequestBuilderSetUrlIsNotNil() {
+        let requestBuilder = UrlRequestBuilder()
+            .setUrl(url: testOnlyUrl)
+        
+        XCTAssertNotNil(requestBuilder.url)
+    }
+    
+    func test_urlRequestBuilderMemberPathIsNil() {
+        let requestBuilder = UrlRequestBuilder()
+        
+        XCTAssertNil(requestBuilder.path)
+    }
+    
+    func test_urlRequestBuilderMemberPathIsNotNil() {
+        let requestBuilder = UrlRequestBuilder(nil, testPath)
+        
+        XCTAssertNotNil(requestBuilder.path)
+    }
+    
+    func test_urlRequestBuilderSetPathIsNil() {
+        let requestBuilder = UrlRequestBuilder()
+            .setPath(path: testPath)
+        
+        XCTAssertNotNil(requestBuilder.path)
+    }
+    
+    func test_urlRequestBuilderSetUrlSetPathNotNil() {
+        let requestBuilder = UrlRequestBuilder()
+            .setUrl(url: testOnlyUrl)
+            .setPath(path: testPath)
+        
+        XCTAssertNotNil(requestBuilder.url)
+        XCTAssertNotNil(requestBuilder.path)
+    }
 }
