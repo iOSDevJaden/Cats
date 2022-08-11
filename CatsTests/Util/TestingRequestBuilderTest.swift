8ec461c1b22262581682154746a7590f0bd4bd5a
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-08-11 20:34:42 +0900

    [Test] UrlRequestBuilder urlQueryItem member variable created

--- CatsTests/Util/TestingRequestBuilderTest.swift
+++ CatsTests/Util/TestingRequestBuilderTest.swift
@@ -11,13 +11,16 @@ struct UrlRequestBuilder {
     var url: URL?
     var path: String?
     var httpMethod: HttpMethod
+    var urlQueryItems: [URLQueryItem]
     
     init(_ url: URL? = nil,
          _ path: String? = nil,
-         _ httpMethod: HttpMethod = .get) {
+         _ httpMethod: HttpMethod = .get,
+         _ urlQueryItems: [URLQueryItem] = []) {
         self.url = url
         self.path = path
         self.httpMethod = httpMethod
+        self.urlQueryItems = urlQueryItems
     }
     
     func setUrl(url: URL?) -> Self {
@@ -34,6 +37,13 @@ struct UrlRequestBuilder {
                           method)
     }
     
+    func setUrlQueryItems(urlQueryItems: [URLQueryItem]) -> Self {
+        UrlRequestBuilder(self.url,
+                          self.path,
+                          self.httpMethod,
+                          urlQueryItems)
+    }
+    
     enum HttpMethod {
         case get
         case post
@@ -45,6 +55,7 @@ struct UrlRequestBuilder {
 class TestingRequestBuilderTest: XCTestCase {
     private let testOnlyUrl = URL(string: "https://example.com")
     private let testPath = "/testPath"
+    private let testUrlQueryItems = [URLQueryItem(name: "name", value: "value1")]
     
     func test_urlRequestBuilderMemberUrlIsNil() {
         let requestBuilder = UrlRequestBuilder()
@@ -121,4 +132,20 @@ class TestingRequestBuilderTest: XCTestCase {
             UrlRequestBuilder.HttpMethod.post,
             requestBuilder.httpMethod)
     }
+    
+    func test_urlRequestBuilderQueryItemsIsEmptyReturnsTrue() {
+        let requestBuilder = UrlRequestBuilder()
+        
+        XCTAssertNotNil(requestBuilder.urlQueryItems)
+        XCTAssertTrue(requestBuilder.urlQueryItems.isEmpty)
+    }
+    
+    func test_urlRequestBuilderQueryItemsIsEmptyReturnsFalse() {
+        let requestBuilder = UrlRequestBuilder()
+            .setUrlQueryItems(urlQueryItems: testUrlQueryItems)
+        
+        XCTAssertNotNil(requestBuilder.urlQueryItems)
+        XCTAssertFalse(requestBuilder.urlQueryItems.isEmpty)
+    }
+    
 }
