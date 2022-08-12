d65325661ec9dbf8090f1e66049d012dfd0064df
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-08-13 06:44:22 +0900

    [Test] BaseRequestBuilder Unit test file created

---
+++ CatsTests/api+services/TestingBaseRequestBuilderTest.swift
@@ -0,0 +1,13 @@
+//
+//  TestingBaseRequestBuilderTest.swift
+//  CatsTests
+//
+//  Created by 김태호 on 2022/08/13.
+//
+
+import XCTest
+@testable import Cats
+
+class TestingBaseRequestBuilderTest: XCTestCase {
+    
+}
