645f62c89ab20ff7f25ea9495f39bee1ded3012c
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-08-15 18:30:57 +0900

    [Refactor] Const Value applied to Test Base API struct.

--- CatsTests/api+services/api/TestBaseApi.swift
+++ CatsTests/api+services/api/TestBaseApi.swift
@@ -9,10 +9,12 @@ import Foundation
 @testable import Cats
 
 struct TestBaseApi: BaseApiProtocol {
-    private let baseUrl = URL(string: "https://example.com")!
-    private let basePath = "/api/version"
+    private let baseUrl = Const.baseUrl!
+    private let basePath = Const.apiVersion
+    
     private let defaultHeader = [
-        "x-api-key": "1234"
+        Const.xApiKey: Const.apiKey,
+        Const.contentType: Const.applicationJson
     ]
     
     func getCommonRequestBuilder() -> RequestBuilder {
