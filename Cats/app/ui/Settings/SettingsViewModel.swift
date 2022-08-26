eb818c0d2e21734e1cfb052c4b4941360c459c0e
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-08-27 05:18:38 +0900

    [Test] Settings ViewModel load latest user preferences test cases added
    - current page
    - number of images per page
    - user id
    - user profile image.

--- Cats/app/ui/Settings/SettingsViewModel.swift
+++ Cats/app/ui/Settings/SettingsViewModel.swift
@@ -7,27 +7,95 @@
 
 import Foundation
 
-class SettingsViewModel: ObservableObject {
-    private let userPreference = UserPreferences.shared
+class SettingsViewModel: BaseViewModel, ObservableObject {
+    private let userPreference: UserPreferences
     
-    func getUserId() -> String {
+    @Published var userId = ""
+    @Published var profileImageData: Data? = nil
+    @Published var page = 0
+    @Published var numberOfImage = 0
+    
+    init(
+        userPreference: UserPreferences = UserPreferences.shared
+    ) {
+        self.userPreference = userPreference
+    }
+    
+    func loadUserId(key: String? = nil) {
+        self.userId = getUserId(key: key)
+    }
+    
+    func loadUserProfileImage(key: String? = nil) {
+        self.profileImageData = getUserProfileImage(key: key)
+    }
+    
+    func loadCurrentPage(key: String? = nil) {
+        self.page = getCurrentPage(key: key)
+    }
+    
+    func loadNumberOfImagePerPage(key: String? = nil) {
+        self.numberOfImage = getNumberOfImage(key: key)
+    }
+    
+    func getUserId(key: String? = nil) -> String {
+        if let key = key {
+            return userPreference.getUserProfileId(key: key)
+        }
         return userPreference.getUserProfileId()
     }
     
     // Current Page
-    func getCurrentPage() -> Int {
+    func getCurrentPage(key: String? = nil) -> Int {
+        if let key = key {
+            return userPreference.getCurrentSearchImagePage(key: key)
+        }
         return userPreference.getCurrentSearchImagePage()
     }
     
-    func setCurrentPage(_ page: Int) {
+    func setCurrentPage(
+        _ page: Int,
+        key: String? = nil
+    ) {
+        if let key = key {
+            userPreference.setCurrentSearchImagePage(
+                page,
+                key: key
+            )
+        }
         userPreference.setCurrentSearchImagePage(page)
     }
     
-    func getNumberOfImage() -> Int {
+    func getNumberOfImage(key: String? = nil) -> Int {
+        if let key = key {
+            return userPreference.getCurrentNumberOfImagePerPage(key: key)
+        }
         return userPreference.getCurrentNumberOfImagePerPage()
     }
     
-    func setNumberOfImage(_ numberOfImage: Int) {
+    func setNumberOfImage(
+        _ numberOfImage: Int,
+        key: String? = nil
+    ) {
+        if let key = key {
+            userPreference.setCurrentNumberOfImagePerPage(numberOfImage, key: key)
+        }
         userPreference.setCurrentNumberOfImagePerPage(numberOfImage)
+    }
+    
+    func getUserProfileImage(key: String? = nil) -> Data {
+        if let key = key {
+            return userPreference.getUserProfileImage(key: key)
+        }
+        return userPreference.getUserProfileImage()
+    }
+    
+    func setUserProfileImage(
+        _ imageData: Data?,
+        key: String? = nil
+    )  {
+        if let key = key {
+            userPreference.setUserProfileImage(imageData: imageData, key: key)
+        }
+        userPreference.setUserProfileImage(imageData: imageData)
     }
 }
--- CatsTests/ViewModels/TestSettingsViewModelTesting.swift
+++ CatsTests/ViewModels/TestSettingsViewModelTesting.swift
@@ -20,9 +20,59 @@ class TestSettingsViewModelTesting: XCTestCase {
     }
     
     override func tearDown() {
+        resetUserProfiles()
         userPreferences = nil
         viewModel = nil
     }
     
+    private func resetUserProfiles() {
+        userPreferences.resetUserProfileId(key: KeysForTest.userDefaultProfileIdKeyForTest)
+        userPreferences.resetUserProfileImage(key: KeysForTest.userDefaultProfileImageKeyForTest)
+        userPreferences.resetCurrentNumberOfImagePerPage(key: KeysForTest.userDefaultCurrentSearchImagePageForTest)
+    }
+    
+    func test_settings_viewModel_load_latest_user_id() {
+        viewModel.userId = ""
+        
+        viewModel.loadUserId(key: KeysForTest.userDefaultProfileIdKeyForTest)
+        
+        XCTAssertEqual(
+            viewModel.userId,
+            userPreferences.getUserProfileId(key: KeysForTest.userDefaultProfileIdKeyForTest)
+        )
+    }
+    
+    func test_settings_viewModel_load_latest_user_profile_image() {
+        viewModel.profileImageData = nil
+        
+        viewModel.loadUserProfileImage(key: KeysForTest.userDefaultProfileImageKeyForTest)
+
+        XCTAssertEqual(
+            viewModel.profileImageData,
+            userPreferences.getUserProfileImage(key: KeysForTest.userDefaultProfileImageKeyForTest)
+        )
+    }
+    
+    func test_settings_viewModel_load_latest_number_of_image_per_page() {
+        viewModel.numberOfImage = 0
+        
+        viewModel.loadNumberOfImagePerPage(key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
+
+        XCTAssertEqual(
+            viewModel.numberOfImage,
+            userPreferences.getCurrentNumberOfImagePerPage(key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
+        )
+    }
+    
+    func test_settings_viewModel_load_latest_page() {
+        viewModel.page = 0
+        
+        viewModel.loadCurrentPage(key: KeysForTest.userDefaultCurrentSearchImagePageForTest)
+
+        XCTAssertEqual(
+            viewModel.page,
+            userPreferences.getCurrentSearchImagePage(key: KeysForTest.userDefaultCurrentSearchImagePageForTest)
+        )
+    }
     
 }
