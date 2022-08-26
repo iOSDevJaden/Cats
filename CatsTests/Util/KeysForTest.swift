bafdaf1fc16be29ff19c9993c25c1c285619b7c9
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-08-27 04:35:44 +0900

    [Refactor] Test User Preference Cache modified not to share the same key values.
    - UserPreference methods shares the same key. Therefore, the test cases affect on the code that actually works.

---
+++ CatsTests/Util/KeysForTest.swift
@@ -0,0 +1,15 @@
+//
+//  KeysForTest.swift
+//  CatsTests
+//
+//  Created by 김태호 on 2022/08/27.
+//
+
+import Foundation
+
+enum KeysForTest {
+    static let userDefaultProfileIdKeyForTest = "User Default ID Key For Test"
+    static let userDefaultProfileImageKeyForTest = "User Default Profile Image Key For Test"
+    static let userDefaultCurrentSearchImagePageForTest = "User Default Current Search Image Page Key For Test"
+    static let userDefaultNumberOfImagePerPageForTest = "User Default Number Of Image Per Page Key For Test"
+}
