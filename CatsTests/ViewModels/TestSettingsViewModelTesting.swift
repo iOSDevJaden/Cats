c51c39df891b94d0ccfad09015d612ea786610b0
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-08-27 12:16:14 +0900

    [Test] Settings ViewModel Test cases added
    - If number of page set out of range, it set to max / min number value.
    - Reset User Preferences.

--- CatsTests/ViewModels/TestSettingsViewModelTesting.swift
+++ CatsTests/ViewModels/TestSettingsViewModelTesting.swift
@@ -78,10 +78,28 @@ class TestSettingsViewModelTesting: XCTestCase {
     func test_settings_viewModel_update_number_of_image_per_page() {
         viewModel.loadNumberOfImagePerPage(key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
         
+        viewModel.setNumberOfImage(30, key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
+
+        viewModel.loadNumberOfImagePerPage(key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
+        XCTAssertEqual(viewModel.numberOfImage, 30)
+    }
+    
+    func test_settings_viewModel_update_number_of_image_per_page_out_of_range_minimum_set_minimum_value() {
+        viewModel.loadNumberOfImagePerPage(key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
+        
+        viewModel.setNumberOfImage(0, key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
+
+        viewModel.loadNumberOfImagePerPage(key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
+        XCTAssertEqual(viewModel.numberOfImage, 15)
+    }
+    
+    func test_settings_viewModel_update_number_of_image_per_page_out_of_range_maximum_set_maximum_value() {
+        viewModel.loadNumberOfImagePerPage(key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
+        
         viewModel.setNumberOfImage(50, key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
 
         viewModel.loadNumberOfImagePerPage(key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
-        XCTAssertEqual(viewModel.numberOfImage, 50)
+        XCTAssertEqual(viewModel.numberOfImage, 30)
     }
     
     func test_settings_viewModel_update_user_profile_image() {
@@ -92,5 +110,25 @@ class TestSettingsViewModelTesting: XCTestCase {
         
         viewModel.loadUserProfileImage(key: KeysForTest.userDefaultProfileImageKeyForTest)
         XCTAssertEqual(viewModel.profileImageData, profileImageData)
+    }
+    
+    func test_settings_viewModel_reset_number_of_image_per_page() {
+        viewModel.setNumberOfImage(20, key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
+
+        viewModel.resetNumberOfImage(key: KeysForTest.userDefaultNumberOfImagePerPageForTest)
+        
+        XCTAssertEqual(viewModel.numberOfImage, 15)
+    }
+    
+
+    func test_settings_viewModel_reset_user_profile_image() {
+        let profileImageData = UIImage(named: "AppIcon")?.jpegData(compressionQuality: 0.5)!
+        viewModel.setUserProfileImage(profileImageData, key: KeysForTest.userDefaultProfileImageKeyForTest)
+        
+        viewModel.resetUserProfileImage(key: KeysForTest.userDefaultProfileImageKeyForTest)
+        let defaultProfileImage = UIImage(named: "cat-paw")?.jpegData(compressionQuality: 0.5)!
+        
+        viewModel.loadUserProfileImage(key: KeysForTest.userDefaultProfileImageKeyForTest)
+        XCTAssertEqual(viewModel.profileImageData, defaultProfileImage)
     }
 }
