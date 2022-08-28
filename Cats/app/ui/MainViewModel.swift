e7bfc96d91e1aa5251e31593b1422b38053528a9
Taeho-Kim <dev.jaden.kim@gmail.com>
2022-08-28 21:15:32 +0900

    [Feat] Main ViewModel class created.

---
+++ Cats/app/ui/MainViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewModel.swift
+//  Cats
+//
+//  Created by 김태호 on 2022/08/28.
+//
+
+import Foundation
+
+class MainViewModel: BaseViewModel, ObservableObject {
+    
+    func getNotificationPermission() {
+        
+    }
+    
+}
